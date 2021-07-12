15a8e33b16bcde4e56e99d369074cd931f14765f
Kae Bartlett <kae@voxelfox.co.uk>
2021-07-12 05:25:12 +0100

    Update the settings menu

--- cogs/bot_settings.py
+++ cogs/bot_settings.py
@@ -1,88 +1,65 @@
 import voxelbotutils as vbu
 import discord
-from discord.ext import commands
 
 
-async def bong_channel_storage_whatever(menu, channel: discord.TextChannel):
-    await vbu.SettingsMenuOption.get_set_guild_settings_callback('guild_settings', 'bong_channel_id')(menu, channel)
+async def bong_channel_storage_whatever(ctx, data):
+    channel = data[0]
+    await vbu.menus.Menu.callbacks.set_table_column(vbu.menus.DataLocation.GUILD, "guild_settings", "bong_channel_id")(ctx, (channel,))
+    ctx.bot.guild_settings[ctx.guild.id]["bong_channel_id"] = channel.id if channel is not None else channel
     if channel is None:
         return
     try:
         webhook = await channel.create_webhook(name="Big Ben")
     except discord.HTTPException:
         return
-    async with menu.context.bot.database() as db:
+    async with ctx.bot.database() as db:
         await db(
             """INSERT INTO guild_settings (guild_id, bong_channel_webhook)
             VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET bong_channel_webhook=$2""",
-            menu.context.guild.id, webhook.url
+            ctx.guild.id, webhook.url,
         )
-    menu.context.bot.guild_settings[menu.context.guild.id]["bong_channel_webhook"] = webhook.url
+    ctx.bot.guild_settings[ctx.guild.id]["bong_channel_webhook"] = webhook.url
 
 
-class BotSettings(vbu.Cog):
-
-    @vbu.group()
-    @commands.has_permissions(manage_guild=True)
-    @commands.bot_has_permissions(send_messages=True, embed_links=True, add_reactions=True)
-    @commands.guild_only()
-    async def setup(self, ctx: vbu.Context):
-        """
-        Run the bot setup.
-        """
-
-        # Make sure it's only run as its own command, not a parent
-        if ctx.invoked_subcommand is not None:
-            return
-
-        # Create settings menu
-        menu = vbu.SettingsMenu()
-        settings_mention = vbu.SettingsMenuOption.get_guild_settings_mention
-        menu.add_multiple_options(
-            vbu.SettingsMenuOption(
-                ctx=ctx,
-                display=lambda c: "Set bong channel (currently {0})".format(settings_mention(c, 'bong_channel_id')),
-                converter_args=(
-                    vbu.SettingsMenuConverter(
-                        prompt="Where do you want all the bong messages to go to?",
-                        asking_for="bong channel",
-                        converter=commands.TextChannelConverter,
-                    ),
-                ),
-                callback=bong_channel_storage_whatever,
+settings_menu = vbu.menus.Menu(
+    vbu.menus.Option(
+        display=lambda ctx: f"Set bong channel (currently {ctx.get_mentionable_channel(ctx.bot.guild_settings[ctx.guild.id]['bong_channel_id'])})",
+        component_display="Set bong channel",
+        converters=[
+            vbu.menus.Converter(
+                prompt="What channel do you want to set as your bong channel?",
+                converter=discord.TextChannel,
             ),
-            vbu.SettingsMenuOption(
-                ctx=ctx,
-                display=lambda c: "Set 'first bong reaction' role (currently {0})".format(settings_mention(c, 'bong_role_id')),
-                converter_args=(
-                    vbu.SettingsMenuConverter(
-                        prompt="Which role should the first reaction to the bong message get?",
-                        asking_for="bong channel",
-                        converter=commands.RoleConverter,
-                    ),
-                ),
-                callback=vbu.SettingsMenuOption.get_set_guild_settings_callback('guild_settings', 'bong_role_id'),
+        ],
+        callback=bong_channel_storage_whatever,
+        cache_callback=None,
+    ),
+    vbu.menus.Option(
+        display=lambda ctx: f"Set bong role (currently {ctx.get_mentionable_role(ctx.bot.guild_settings[ctx.guild.id]['bong_role_id'])})",
+        component_display="Set bong role",
+        converters=[
+            vbu.menus.Converter(
+                prompt="What role should people get if they're the first to click the bong button?",
+                converter=discord.Role,
             ),
-            vbu.SettingsMenuOption(
-                ctx=ctx,
-                display=lambda c: "Set bong reaction emoji (currently {0})".format(c.bot.guild_settings[c.guild.id]['bong_emoji']),
-                converter_args=(
-                    vbu.SettingsMenuConverter(
-                        prompt="What should emoji should be added to each bong message?",
-                        asking_for="bong emoji",
-                        converter=str,
-                    ),
-                ),
-                callback=vbu.SettingsMenuOption.get_set_guild_settings_callback('guild_settings', 'bong_emoji'),
+        ],
+        callback=vbu.menus.Menu.callbacks.set_table_column(vbu.menus.DataLocation.GUILD, "guild_settings", "bong_role_id"),
+        cache_callback=vbu.menus.Menu.callbacks.set_cache_from_key(vbu.menus.DataLocation.GUILD, "bong_role_id"),
+    ),
+    vbu.menus.Option(
+        display=lambda ctx: f"Set bong emoji (currently {ctx.bot.guild_settings[ctx.guild.id]['bong_emoji'] or 'null'})",
+        component_display="Set bong emoji",
+        converters=[
+            vbu.menus.Converter(
+                prompt="What should emoji should be added to each bong message?",
+                converter=str,
             ),
-        )
-        try:
-            await menu.start(ctx)
-            await ctx.send("Done setting up!")
-        except (vbu.errors.InvokedMetaCommand, discord.HTTPException):
-            pass
+        ],
+        callback=vbu.menus.Menu.callbacks.set_table_column(vbu.menus.DataLocation.GUILD, "guild_settings", "bong_emoji"),
+        cache_callback=vbu.menus.Menu.callbacks.set_cache_from_key(vbu.menus.DataLocation.GUILD, "bong_emoji"),
+    ),
+)
 
 
 def setup(bot: vbu.Bot):
-    x = BotSettings(bot)
-    bot.add_cog(x)
+    settings_menu.create_cog(bot)
